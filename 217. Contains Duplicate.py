36ca4961ae40af3eb9fe201d183678d2ea0d8b0e
Asma Karakra <78242119+AsmaKarakra@users.noreply.github.com>
2021-04-25 14:05:04 -0700

    Update 217. Contains Duplicate.py

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -9,6 +9,23 @@ Example 1:
 Input: nums = [1,2,3,1]
 Output: true
 
+
+Assumptions: 
+-	Array is not sorted 
+-	Can have positive and negative integers 
+-	Can modify arrays 
+-	Value can repeat more than twice 
+
+Method 1: 
+Sort the array and use two pointers to check if two values next to each other match. 
+Time: N Log(N) | Space: O(1)
+
+Method 2: Two nested for loops with pointers to traverse through the array and check every value 
+Time: O(N^2) | Space: O(1)
+
+Method 3: Hash Table to store values and check if they are repeated. 
+Time: O(n) }| Space O(n) 
+
 """
 
 #solution: 
